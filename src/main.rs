ee9f18bbf8b92065723eeece7ff73f42e07f957b
Mrp1Dev <mrp1dev@gmail.com>
2021-03-02 18:12:52 +0530

    gravity

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,13 @@ fn main() {
     App::build()
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup.system())
+        .add_system(gravity_system.system())
         .add_system(velocity_system.system())
         .run();
 }
 
 struct Velocity(Vec3);
+struct Gravity(f32);
 struct Bird;
 
 enum Collider {
@@ -34,17 +36,20 @@ fn setup(
             sprite: Sprite::new(Vec2 { x: 25.0, y: 25.0 }),
             ..Default::default()
         })
-        .with(Velocity(Vec3 {
-            y: -100.0,
-            x: 0.0,
-            z: 0.0,
-        }))
-        .with(Collider::Bird);
+        .with(Velocity(Vec3::zero()))
+        .with(Collider::Bird)
+        .with(Gravity(125.0));
 }
 
 fn velocity_system(time: Res<Time>, mut query: Query<(&Velocity, &mut Transform)>) {
     for (vel, mut transform) in query.iter_mut() {
         let translation = &mut transform.translation;
         *translation += time.delta_seconds() * vel.0;
+    }
+}
+
+fn gravity_system(time: Res<Time>, mut query: Query<(&mut Velocity, &Gravity)>) {
+    for (mut vel, gravity) in query.iter_mut() {
+        vel.0.y -= gravity.0 * time.delta_seconds();
     }
 }
